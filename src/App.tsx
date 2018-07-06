8987e82bd38c96365f341cca82afdd51911656e6
knjname <pc@knj.name>
2018-07-06 13:17:04 +0900

    Init files

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import "./App.css";
+
+import * as Plottable from "plottable";
+import * as React from "react";
+
+class App extends React.Component {
+  private divElem: React.RefObject<HTMLDivElement> = React.createRef();
+
+  public render() {
+    return <div style={{ width: 500, height: 300 }} ref={this.divElem} />;
+  }
+
+  componentDidMount() {
+    const xScale = new Plottable.Scales.Linear();
+    const yScale = new Plottable.Scales.Linear();
+
+    const xAxis = new Plottable.Axes.Numeric(xScale, "bottom");
+    const yAxis = new Plottable.Axes.Numeric(yScale, "left");
+
+    const plot = new Plottable.Plots.Line();
+    plot.x((d: typeof data[0]) => d.x, xScale);
+    plot.y((d: typeof data[0]) => d.y, yScale);
+
+    const data = [
+      { x: 0, y: 1 },
+      { x: 1, y: 2 },
+      { x: 2, y: 4 },
+      { x: 3, y: 8 }
+    ];
+
+    const dataset = new Plottable.Dataset(data);
+    plot.addDataset(dataset);
+
+    const chart = new Plottable.Components.Table([
+      [yAxis, plot],
+      [null, xAxis]
+    ]);
+
+    chart.renderTo(this.divElem.current as any);
+  }
+}
+
+export default App;
